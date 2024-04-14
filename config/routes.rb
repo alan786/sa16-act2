bb4addde0ff4dc0ab72da2bf05276929de28b27f
Alan Ren <alanlin786@gmail.com>
2024-04-13 19:28:46 -0500

    the portfolio page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  #get 'home', to: 'pages#home', as: 'home'
+  root 'pages#home'
+  get 'about', to: 'pages#about', as: 'about'
+  get 'work', to: 'pages#work', as: 'work'
+  get 'contact', to: 'pages#contact', as: 'contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
